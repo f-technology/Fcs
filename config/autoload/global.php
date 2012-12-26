b193c62ffbaf3e9008cbb92efb8ccf6de45a11f3
zer0c14 <zer0c14@gmail.com>
2012-12-26 17:53:48 +0400

    Move configuration from database.global.php in global.php

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -12,4 +12,16 @@
  */
 
 return array(
+    'db' => array(
+        'driver' => 'Pdo',
+        'dsn' => 'mysql:dbname=fcs;hostname=localhost',
+        'driver_options' => array(
+            PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"
+        ),
+    ),
+    'service_manager' => array(
+        'factories' => array(
+            'Zend\Db\Adapter\Adapter' => 'Zend\Db\Adapter\AdapterServiceFactory',
+        ),
+    ),
 );
