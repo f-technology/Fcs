a5521f648fec0ed0caf2fa328b58f6ad2a7fd778
zer0c14 <zer0c14@gmail.com>
2012-12-27 14:19:07 +0400

    Add "MteBase" library in "Application" module

--- module/Application/Module.php
+++ module/Application/Module.php
@@ -1,12 +1,11 @@
 <?php
 namespace Application;
 
+use MteBase\Module\AbstractModule;
 use Zend\Mvc\ModuleRouteListener;
-use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
-use Zend\ModuleManager\Feature\ConfigProviderInterface;
 use Zend\Mvc\MvcEvent;
 
-class Module implements AutoloaderProviderInterface, ConfigProviderInterface
+class Module extends AbstractModule
 {    
     public function onBootstrap(MvcEvent $e)
     {
@@ -15,24 +14,5 @@ class Module implements AutoloaderProviderInterface, ConfigProviderInterface
         $moduleRouteListener->attach(
             $e->getApplication()->getEventManager()
         );
-    }
-    
-    public function getConfig()
-    {
-        return include __DIR__ . '/config/module.config.php';
-    }
-    
-    public function getAutoloaderConfig()
-    {
-        return array(
-            'Zend\Loader\ClassMapAutoloader' => array(
-                __DIR__ . '/autoload_classmap.php',
-            ),
-            'Zend\Loader\StandardAutoloader' => array(
-                'namespaces' => array(
-                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
-                ),
-            ),
-    	);
     }
 }
