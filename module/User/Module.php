7c0977980634f148ec75b23c9ef04e7a01649bae
zer0c14 <zer0c14@gmail.com>
2012-12-28 14:27:37 +0400

    Add form event example in "User" module

--- module/User/Module.php
+++ module/User/Module.php
@@ -1,8 +1,26 @@
 <?php
 namespace User;
 
+use Zend\EventManager\EventInterface;
 use MteBase\Module\AbstractModule;
 
 class Module extends AbstractModule
 {
+    public function onBootstrap(EventInterface $e)
+    {
+        // form event example
+        $events = $e->getApplication()->getEventManager()->getSharedManager();
+        $events->attach('ZfcUser\Form\Register','init', function($e) {
+            $form = $e->getTarget();
+            $form->add(array(
+                'name' => 'name',
+                'options' => array(
+                    'label' => 'Firstname/Lastname',
+                ),
+                'attributes' => array(
+                    'type' => 'text'
+                ),
+            ));
+        });
+    }
 }
